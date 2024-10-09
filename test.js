aff54d5d76c59007f538a3e5e2c7135c507bb633
michael <doctor.d.tennant@gmail.com>
2024-10-09 12:12:04 +0200

    js

---
+++ test.js
@@ -0,0 +1,25 @@
+let book= {
+    title: "Empire of the Vampire",
+    characters: ["Gabriel de Léon", "Dior Lachance", "Celene Castia", "Forever King", "Aaron De Coste"],
+    publishing_date: 2021,
+    read: true,
+    pages: 864,
+    synopsis: "It has been twenty-seven long years since the last sunrise. For nearly three decades, vampires have waged war against humanity; building their eternal empire even as they tear down our own. Now, only a few tiny sparks of light endure in a sea of darkness. Gabriel de León is a silversaint: a member of a holy brotherhood dedicated to defending realm and church from the creatures of the night. But even the Silver Order could not stem the tide once daylight failed us, and now, only Gabriel remains. Imprisoned by the very monsters he vowed to destroy, the last silversaint is forced to tell his story. A story of legendary battles and forbidden love, of faith lost and friendships won, of the Wars of the Blood and the Forever King and the quest for humanity’s last remaining hope: The Holy Grail.",
+    series: true,
+    signed: true,
+    appropriate_for_children: false,
+    genres: ["Fantasy", "Horror"]
+}
+
+let album={
+    title: "Get Your Heart On!",
+    artist: "Simple Plan",
+    release_date: "21 June 2011",
+    band_members: ["Pierre Bouvier", "Sébastien Lefebvre", "Jeff Stinco", "Chuck Comeau", "David Desrosiers"],
+    track_numbers: 11,
+    track_names: ["You Suck at Love", "Can't Keep My Hands Off You", "Jet Lag (ft. Natasha Bedingfield)", "Astronaut", "Loser of the Year", "Anywhere Else But Here", "Freaking Me Out", "Summer Paradise", "Gone Too Soon", "Last One Standing", "This Song Saved My Life"],
+    listened: true,
+    genre: "Pop Rock",
+    length: "37:45",
+    label: "Atlantic"
+}
