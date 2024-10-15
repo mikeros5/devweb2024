43a54d3a70ec05e3941257c9fd001deac5cf30e2
michael <doctor.d.tennant@gmail.com>
2024-10-15 18:12:01 +0200

    js

--- test.js
+++ test.js
@@ -1,15 +1,32 @@
 let book= {
     title: "Empire of the Vampire",
-    characters: ["Gabriel de Léon", "Dior Lachance", "Celene Castia", "Forever King", "Aaron De Coste"],
-    publishing_date: 2021,
+    author: "Jay Kristoff",
+    publisher: "Harper Voyage",
+    publication_date: "7 September 2021",
+    publication_place: "Australia",
+    genres: ["Fantasy", "Horror"],
+    appropriate_for_children: false,
     read: true,
-    pages: 864,
+    isbn: "978-0-00-835043-7",
+    edition: "Paperback",
+    pages: 719,
+    parts: 6,
+    chapters_per_parts: {
+      part_1: 11,
+      part_2: 12,
+      part_3: 13,
+      part_4: 17,
+      part_5: 20,
+      part_6: 28,
+},
     synopsis: "It has been twenty-seven long years since the last sunrise. For nearly three decades, vampires have waged war against humanity; building their eternal empire even as they tear down our own. Now, only a few tiny sparks of light endure in a sea of darkness. Gabriel de León is a silversaint: a member of a holy brotherhood dedicated to defending realm and church from the creatures of the night. But even the Silver Order could not stem the tide once daylight failed us, and now, only Gabriel remains. Imprisoned by the very monsters he vowed to destroy, the last silversaint is forced to tell his story. A story of legendary battles and forbidden love, of faith lost and friendships won, of the Wars of the Blood and the Forever King and the quest for humanity’s last remaining hope: The Holy Grail.",
+     characters: ["Gabriel de Léon", "Dior Lachance", "Celene Castia", "Forever King", "Aaron De Coste"],
     series: true,
     signed: true,
-    appropriate_for_children: false,
-    genres: ["Fantasy", "Horror"]
+    illustrated: true,
+    price_gbp: 18.99, 
 }
+
 
 let album={
     title: "Get Your Heart On!",
